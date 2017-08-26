9d5a3ae552863c2c946f4d98c9d8cb04a6eee636
Luke Schoen <ltfschoen@gmail.com>
2017-08-26 23:52:28 +1000

    feat: Add Flask server template

---
+++ README.md
@@ -0,0 +1,11 @@
+## Core AI
+
+* Run API server
+  * Install dependencies `pip install -r requirements.txt`
+  * Run `python api/server.py`
+  * Note: Server loads at http://localhost:5000
+
+* Run in development
+  * Install dependencies `npm install`
+  * Run `http-server`
+  * Open in browser `http://127.0.0.1:8080`
--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
+$(".analyse").click(function() {
+  var base64Data = $('.awesome-cropper').children('img').attr('src');
+  $.ajax({
+    url: "/api/todo",
+    type: 'GET',
+    data: base64Data,
+    success: function (response) {
+      $('.analyse-status').html('<strong>Success</strong>');
+      console.log("success: ", response);
+    },
+    complete: function (response) {
+      $('.analyse-status').html('<strong>Complete</strong>');
+      console.log("complete: ", response);
+    },
+    error: function (error) {
+      $('.analyse-status').html('<strong>Error</strong>');
+      console.log("error: ", error);
+    }
+  });
+});
+
+$("#demo").click(function () {
+  $('.analyse-status').html('');
+});
+
 $(document).ready(function () {
   $('#demo').awesomeCropper({
     width: 200,
     height: 1000,
     debug: true
   });
+  $('.analyse-status').html('');
 });
