5095991d7c121246a9709572b63375a1b3584f3b
Luke Schoen <ltfschoen@gmail.com>
2017-08-27 13:54:41 +1000

    feat: Add fake image to fine tune and temporarily remove api request

--- js/script.js
+++ js/script.js
@@ -1,36 +1,54 @@
-var showSpinner = function() { $('.loading-icon').show(); }
-var hideSpinner = function() { $('.loading-icon').hide(); }
+var showSpinner = function() {
+  $('.loading-icon').show();
+}
+var hideSpinner = function() {
+  $('.loading-icon').hide();
+}
 
-// Scroll event handlers
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 140) {
-    $('header').fadeOut();
-  }
-});
+var hideSpinnerSlow = function() {
+  setTimeout(function() {
+    $('.loading-icon').hide();
+  }, 2000);
+}
+
+var hideFakeImage = function() { $('#results').fadeOut(); }
+var showFakeImage = function() { $('#results').fadeIn(); }
+
+// // Scroll event handlers
+// $(window).scroll(function() {
+//   if ($(this).scrollTop() > 140) {
+//     $('header').fadeOut();
+//   }
+// });
 
 $(".analyse").click(function() {
   var base64Data = $('.awesome-cropper').children('img').attr('src');
   showSpinner();
-  $.ajax({
-    url: "http://127.0.0.1:5000/api/v1.0/image", // Flask API endpoint
-    type: 'POST',
-    data: base64Data,
-    success: function (response) {
-      $('#results').html('<strong>' + response + '</strong>');
-      console.log("success: ", response);
-      hideSpinner();
-    },
-    complete: function (response) {
-      $('#results').html('<strong>' + response + '</strong>');
-      console.log("complete: ", response);
-      hideSpinner();
-    },
-    error: function (error) {
-      $('#results').html('<strong>' + response + '</strong>');
-      console.log("error: ", error);
-      hideSpinner();
-    }
-  });
+
+  showFakeImage();
+
+  hideSpinnerSlow();
+
+  // $.ajax({
+  //   url: "http://127.0.0.1:5000/api/v1.0/image", // Flask API endpoint
+  //   type: 'POST',
+  //   data: base64Data,
+  //   success: function (response) {
+  //     $('#results').html('<strong>' + response + '</strong>');
+  //     console.log("success: ", response);
+  //     hideSpinner();
+  //   },
+  //   complete: function (response) {
+  //     $('#results').html('<strong>' + response + '</strong>');
+  //     console.log("complete: ", response);
+  //     hideSpinner();
+  //   },
+  //   error: function (error) {
+  //     $('#results').html('<strong>' + response + '</strong>');
+  //     console.log("error: ", error);
+  //     hideSpinner();
+  //   }
+  // });
 });
 
 $("#demo").click(function () {
@@ -38,11 +56,12 @@ $("#demo").click(function () {
 });
 
 $(document).ready(function () {
+  hideFakeImage();
   hideSpinner();
   $('#demo').awesomeCropper({
     width: 200,
     height: 1000,
     debug: true
   });
-  $('#results').html('');
+  // $('#results').html('');
 });
